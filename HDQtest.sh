b88b1666b076d4c45c0d3aefff6804c18b31f186
AhmedHussKhalifa <eng.ahmed_hussein@hotmail.com>
2022-07-03 00:38:02 -0400

    added all modifications

--- HDQtest.sh
+++ HDQtest.sh
@@ -1,30 +1,30 @@
 export root="/home/h2amer/AhmedH.Salamah/ilsvrc2012"
 
 # Resize then Compress
-for model in VGG11
-do
-	# for QF_YC in `seq 10 5 100`
-	for QF_YC in 10
-	do
-		export file=./Resize_Compress/HDQ/SWX/${model}/${model}_QF${QF_YC}_SWX.txt
-		# export file=./Resize_Compress/HDQ/YUV/${model}/${model}_QF${QF_YC}_YUV.txt
-		echo ${file}
-		python3 HDQtest_dataloader.py --Model ${model} --J 4 --a 4 --b 4 --QF_Y ${QF_YC} --QF_C ${QF_YC} \
-									  -resize_compress --output_txt ${file} --device "cuda:0"  --root ${root}
-	done
-done
-
-# --> Compress_Resize
-# for model in VGG11
+# for model in Squeezenet
 # do
-# 	for QF_YC in `seq 100 -5 10`
-# 	# for QF_YC in 100
+# 	for QF_YC in `seq 10 5 100`
+# 	# for QF_YC in 10
 # 	do
-# 		# export file=./Compress_Resize/YUV/HDQ/${model}/${model}_QF${QF_YC}_YUV.txt
-# 		export file=./Compress_Resize/HDQ/SWX/${model}/${model}_QF${QF_YC}_SWX.txt
+# 		# export file=./Resize_Compress/HDQ/SWX/${model}/${model}_QF${QF_YC}_SWX.txt
+# 		export file=./Resize_Compress/HDQ/YUV/${model}/${model}_QF${QF_YC}_YUV.txt
 # 		echo ${file}
-# 				python3 HDQtest_dataloader.py --Model ${model} --J 4 --a 4 --b 4 --QF_Y ${QF_YC} --QF_C ${QF_YC} \
-# 									  --output_txt ${file} --device "cuda:0" --root ${root}
+# 		python3 HDQtest_dataloader.py --Model ${model} --J 4 --a 4 --b 4 --QF_Y ${QF_YC} --QF_C ${QF_YC} \
+# 									  -resize_compress --output_txt ${file} --device "cuda:0"  --root ${root}
 # 	done
 # done
+
+# --> Compress_Resize
+for model in VGG11
+do
+	for QF_YC in `seq 100 -5 10`
+	# for QF_YC in 100
+	do
+		# export file=./Compress_Resize/YUV/HDQ/${model}/${model}_QF${QF_YC}_YUV.txt
+		export file=./Compress_Resize/HDQ/SWX/${model}/${model}_QF${QF_YC}_SWX.txt
+		echo ${file}
+				python3 HDQtest_dataloader.py --Model ${model} --J 4 --a 4 --b 4 --QF_Y ${QF_YC} --QF_C ${QF_YC} \
+									  --output_txt ${file} --device "cuda:0" --root ${root}
+	done
+done
 
