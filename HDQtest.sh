6c2b253b95a77e3b3dc416a13e0f2e0e127b2056
AhmedHussKhalifa <eng.ahmed_hussein@hotmail.com>
2022-08-01 23:42:26 -0400

    add module to python

--- HDQtest.sh
+++ HDQtest.sh
@@ -42,45 +42,45 @@ export root="/home/h2amer/AhmedH.Salamah/ilsvrc2012"
 
 # //////////////////////////////////////////
 
-
+DT_Y=('50' '150' '200' '300')
+DT_C=('80' '200' '300' '400')
+export colorspace=0
 
 # Resize then Compress
+for model in VGG11 
+do
+	# for QF_YC in 85
+	# for QF_YC in `seq 100 -5 10`
+	# do
+	for i in "${!DT_Y[@]}"; do
+
+		# export file=./Resize_Compress/HDQ/SWX444/${model}/${model}_QF${QF_YC}_SWX_${colorspace}.txt
+		export file=./Resize_Compress/HDQ_OptD/${model}/YUV/${model}_DT_Y${DT_Y[i]}_DT_C${DT_C[i]}_YUV.txt
+		echo ${file}
+		python3 HDQ_OptD_dataloader.py --Model ${model} --J 4 --a 4 --b 4 \
+									  -resize_compress --colorspace ${colorspace} \
+									  --Qmax_Y 46 --Qmax_C 46 --DT_Y ${DT_Y[i]} --DT_C ${DT_C[i]} \
+									  --output_txt ${file} --device "cuda:0" --root ${root}
+
+	done
+done
+
+
+
 # for model in VGG11 
 # do
-# 	for QF_YC in 85
 # 	# for QF_YC in `seq 100 -5 10`
-# 	do
-# 		for colorspace in 0
-# 		do
+# 	# do
+# 	for i in "${!DT_Y[@]}"; do
+# 		for colorspace in 2; do
 # 			# export file=./Resize_Compress/HDQ/SWX444/${model}/${model}_QF${QF_YC}_SWX_${colorspace}.txt
-# 			export file=./Resize_Compress/HDQ_OptD/${model}/YUV/${model}_QF${QF_YC}_YUV.txt
+# 			export file=./Resize_Compress/HDQ_OptD/${model}/SWX/${model}_DT_Y${DT_Y[i]}_DT_C${DT_C[i]}_SWX.txt
+# 			printf '${DT_Y[%s]}=%s\t%s\n' "$i" "${DT_Y[i]}" "${DT_C[i]}"
 # 			echo ${file}
 # 			python3 HDQ_OptD_dataloader.py --Model ${model} --J 4 --a 4 --b 4 \
 # 										  -resize_compress --colorspace ${colorspace} \
-# 										  --Qmax_Y 46 --Qmax_C 46 --DT_Y 2 --DT_C 5  \
+# 										  --Qmax_Y 46 --Qmax_C 46 --DT_Y ${DT_Y[i]} --DT_C ${DT_C[i]} \
 # 										  --output_txt ${file} --device "cuda:0" --root ${root}
 # 		done
 # 	done
 # done
-
-
-DT_Y=('50' '150' '200' '300')
-DT_C=('80' '200' '300' '400')
-
-for model in VGG11 
-do
-	# for QF_YC in `seq 100 -5 10`
-	# do
-	for i in "${!DT_Y[@]}"; do
-		for colorspace in 2; do
-			# export file=./Resize_Compress/HDQ/SWX444/${model}/${model}_QF${QF_YC}_SWX_${colorspace}.txt
-			export file=./Resize_Compress/HDQ_OptD/${model}/SWX/${model}_DT_Y${DT_Y[i]}_DT_C${DT_C[i]}_SWX.txt
-			printf '${DT_Y[%s]}=%s\t%s\n' "$i" "${DT_Y[i]}" "${DT_C[i]}"
-			echo ${file}
-			python3 HDQ_OptD_dataloader.py --Model ${model} --J 4 --a 4 --b 4 \
-										  -resize_compress --colorspace ${colorspace} \
-										  --Qmax_Y 46 --Qmax_C 46 --DT_Y ${DT_Y[i]} --DT_C ${DT_C[i]} \
-										  --output_txt ${file} --device "cuda:0" --root ${root}
-		done
-	done
-done
